848c99f2b79829e85ec6228830e8d2a24a244e2f
VS-LIU <vctl95@gmail.com>
2024-03-18 23:18:29 -0700

    Switch over to online DBs

--- database/db_utils.js
+++ database/db_utils.js
@@ -7,8 +7,8 @@ async function printMySQLVersion() {
 
 	try {
 		const results = await database.query(sqlQuery);
-		// console.log(`Successfully connected to MySQL [user: ${process.env.MYSQL_USER}, database: ${process.env.MYSQL_DATABASE}]`);
-		console.log(`Successfully connected to MySQL [user: ${process.env.MYSQL_USER_LOCAL}, database: ${process.env.MYSQL_DATABASE_LOCAL}]`);
+		console.log(`Successfully connected to MySQL [user: ${process.env.MYSQL_USER}, database: ${process.env.MYSQL_DATABASE}]`);
+		// console.log(`Successfully connected to MySQL [user: ${process.env.MYSQL_USER_LOCAL}, database: ${process.env.MYSQL_DATABASE_LOCAL}]`);
 		console.log(results[0]);
 		return true;
 	}
